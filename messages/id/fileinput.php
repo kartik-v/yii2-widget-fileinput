1cd6e0f551c10f58d6b0189cf5a7c7f4be34890c
muhammadcahya <muhammadcahyax@gmail.com>
2015-07-20 14:47:29 +0700

    Add Indonesian translations

---
+++ messages/id/fileinput.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Message translations for \kartik\widgets\FileInput.
+ *
+ * It contains the localizable messages extracted from source code.
+ * You may modify this file by translating the extracted messages.
+ *
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * Message string can be used with plural forms format. Check i18n section
+ * of the guide for details.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Note:' => 'Catatan:',
+    'multiple file upload' => 'multiple file upload',
+    'file preview and multiple file upload' => 'file preview dan multiple file upload',
+    'Your browser does not support {validation}. Try an alternative or more recent browser to access these features.' => 'Browser anda tidak mendukung {validation}. Cobalah browser alternatif atau yang lebih baru, untuk mengakses fitur ini.'
+];
